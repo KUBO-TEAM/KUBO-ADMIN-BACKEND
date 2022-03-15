86a3ddada8ed2046164db7119d582ef64a62b46c
ecoknows <jerico.villaraza19@gmail.com>
2022-03-15 22:41:55 +0800

    :recycle: test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,10 @@ import { AppComponent } from './app.component';
 import { ApiAuthenticationModule } from './modules/api-authentication/api-authentication.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RecipeCrudModule } from './modules/recipe-crud/recipe-crud.module';
-import { DevSidenavComponent } from './dev/components/dev-sidenav/dev-sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DevSidenavComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
