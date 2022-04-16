506ab30d92a4ea56d5d0e3f8dc01e19ac60d709e
ecoknows <jerico.villaraza19@gmail.com>
2022-04-16 14:35:10 +0800

    :bug: checkbox padding

--- src/app/modules/recipe-crud/presentation/recipe-table/recipe-table.component.ts
+++ src/app/modules/recipe-crud/presentation/recipe-table/recipe-table.component.ts
@@ -29,7 +29,7 @@ export class RecipeTableComponent implements AfterViewInit, OnChanges {
   dataSource = new MatTableDataSource<RecipeModel>();
   selection = new SelectionModel<RecipeModel>(true, []);
 
-  overviewTableColumns: string[] = ['select', 'name', 'description', 'reference', 'categories', 'action'];
+  overviewTableColumns: string[] = ['select', 'name', 'description', 'reference', 'categories', 'view'];
 
   constructor(
     private recipeServices: RecipeService,
